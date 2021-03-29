3fd8da33953eed51d56a2e81e1b16e6189ab4136
Zama Khan Mohammed <zamakhan_mohammed@apple.com>
2021-03-29 09:32:38 -0500

    update component

--- packages/project-b-mar-29/src/components/my-component/my-component.tsx
+++ packages/project-b-mar-29/src/components/my-component/my-component.tsx
@@ -1,10 +1,10 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, h, Prop } from "@stencil/core";
+import { format } from "../../utils/utils";
 
 @Component({
-  tag: 'my-component',
-  styleUrl: 'my-component.css',
-  shadow: true
+  tag: "my-component",
+  styleUrl: "my-component.css",
+  shadow: true,
 })
 export class MyComponent {
   /**
@@ -27,6 +27,11 @@ export class MyComponent {
   }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()} <a-button myBtnLabel="bar">Foo</a-button></div>;
+    return (
+      <div>
+        Hello, World!! I'm {this.getText()}{" "}
+        <a-button myBtnLabel="bar">Foo</a-button>
+      </div>
+    );
   }
 }
